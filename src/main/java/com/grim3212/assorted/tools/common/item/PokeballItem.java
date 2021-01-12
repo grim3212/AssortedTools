bf89324043b23bf1253fdb6f1c41c89f1d56af18
Dakota Pierone <dakotapierone@yahoo.com>
2021-01-11 17:03:03 -0800

    Add support for custom names for captured entities in Pokeballs

--- src/main/java/com/grim3212/assorted/tools/common/item/PokeballItem.java
+++ src/main/java/com/grim3212/assorted/tools/common/item/PokeballItem.java
@@ -2,6 +2,7 @@ package com.grim3212.assorted.tools.common.item;
 
 import java.util.List;
 
+import com.grim3212.assorted.tools.AssortedTools;
 import com.grim3212.assorted.tools.common.entity.PokeballEntity;
 import com.grim3212.assorted.tools.common.handler.ToolsConfig;
 import com.grim3212.assorted.tools.common.util.NBTHelper;
@@ -16,7 +17,9 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.util.text.IFormattableTextComponent;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 
@@ -54,13 +57,28 @@ public class PokeballItem extends Item {
 	public void addInformation(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
 		if (NBTHelper.hasTag(stack, "StoredEntity")) {
 			CompoundNBT stored = NBTHelper.getTag(stack, "StoredEntity");
+
+			String entityName = stored.getString("id");
 			if (stored.contains("pokeball_name")) {
-				tooltip.add(new TranslationTextComponent("tooltip.pokeball.stored", new TranslationTextComponent(stored.getString("pokeball_name"))));
+				entityName = stored.getString("pokeball_name");
+			}
+
+			if (stored.contains("CustomName")) {
+				String s = stored.getString("CustomName");
+
+				try {
+					IFormattableTextComponent customName = ITextComponent.Serializer.getComponentFromJson(stored.getString("CustomName"));
+					customName.mergeStyle(TextFormatting.BLUE, TextFormatting.ITALIC);
+					tooltip.add(new TranslationTextComponent("tooltip.pokeball.stored_custom_name", customName, new TranslationTextComponent(entityName).mergeStyle(TextFormatting.AQUA)));
+				} catch (Exception exception) {
+					AssortedTools.LOGGER.warn("Failed to parse entity custom name {}", s, exception);
+					tooltip.add(new TranslationTextComponent("tooltip.pokeball.stored", new TranslationTextComponent(entityName).mergeStyle(TextFormatting.AQUA)));
+				}
 			} else {
-				tooltip.add(new TranslationTextComponent("tooltip.pokeball.stored", stored.get("id")));
+				tooltip.add(new TranslationTextComponent("tooltip.pokeball.stored", new TranslationTextComponent(entityName).mergeStyle(TextFormatting.AQUA)));
 			}
 		} else {
-			tooltip.add(new TranslationTextComponent("tooltip.pokeball.empty"));
+			tooltip.add(new TranslationTextComponent("tooltip.pokeball.empty").mergeStyle(TextFormatting.GRAY));
 		}
 	}
 
